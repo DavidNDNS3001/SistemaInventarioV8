a89a8f503a250f3a33b546cd60a92e312e540b3b
DavidNDNS3001 <ndns.david@gmail.com>
2024-06-17 16:07:30 -0500

    Crear Proyecto

---
+++ SistemasInventario/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemasInventario.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
